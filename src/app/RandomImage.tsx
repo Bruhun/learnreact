98a0ec16e5dfd365322db82ae3094c611d106faf
Bruhun <eaerkmenLinkedin@hotmail.com>
2025-04-15 22:35:54 +0300

    Added gradient borders and slight padding

--- src/app/RandomImage.tsx
+++ src/app/RandomImage.tsx
@@ -9,44 +9,50 @@ type RandomImageProps = {
 export default function RandomImage({ imagePaths }: RandomImageProps) {
   const [selectedImage, setSelectedImage] = useState("");
   const [brightness, setBrightness] = useState("");
-  const [golden, setGolden] = useState(false);
-  const [prism, setPrism] = useState(false);
+  const [borderImageSource, setBorderImageSource] = useState("linear-gradient(to right,maroon,maroon)");
 
   useEffect(() => {
     const randIndex = Math.floor(Math.random() * imagePaths.length);
     setSelectedImage(imagePaths[randIndex]);
-    
+
+    const chance = Math.random() * 100;
+
     const randBrightness = Math.random() * 0.8 + 0.2;
     setBrightness(`brightness(${randBrightness})`);
-    
-    if (randBrightness > 0.95) {
-      setPrism(true);
-    } else if (randBrightness > 0.9) {
-      setGolden(true);
+
+    let newBorderGradient = "linear-gradient(to right,gray,maroon)";
+    if (randBrightness > 0.90) {
+      newBorderGradient = "linear-gradient(to right,blue,green)";
+    } else if (randBrightness > 0.7) {
+      newBorderGradient = "linear-gradient(to right,rgb(255, 0, 0),rgb(255, 215, 0))";
+    } else if (chance > 95) {
+      newBorderGradient = "linear-gradient(to right,rgb(0, 255, 234),rgb(255, 0, 255))";
     }
+    setBorderImageSource(newBorderGradient);
+
   }, [imagePaths]);
 
+
   if (!selectedImage) {
-    return <div style={{ height: "500px", width: "500px", margin: "20px auto" }}></div>;
+    return <div style={{ height: "508px", width: "508px", margin: "20px auto", border: "4px solid transparent" }}></div>;
   }
 
   return (
-    <Image 
-      src={selectedImage} 
-      alt="Fatih Cem" 
-      width={500} 
-      height={500} 
+    <Image
+      src={selectedImage}
+      alt="Fatih Cem"
+      width={500}
+      height={500}
       style={{
         filter: brightness,
-        border: prism 
-          ? "3px solid rgb(67, 220, 255)" 
-          : golden 
-            ? "2px solid gold" 
-            : "none",
+        border: "4px solid transparent",
+        borderImageSource: borderImageSource,
+        borderImageSlice: 1,
         display: "block",
         margin: "20px auto",
-        maxWidth: "100%"
-      }} 
+        maxWidth: "100%",
+        padding: "4px"
+      }}
     />
   );
 }
