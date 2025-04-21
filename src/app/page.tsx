feb039d886f6f3d1e4fd58ec5c478888790ece8a
Bruhun <eaerkmenLinkedin@hotmail.com>
2025-04-21 13:56:26 +0300

    added animated background and variants.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,15 @@ export default function Home() {
   const imagePaths = getImagePaths();
 
   return (
-    <div style={{display: "flex",flexDirection: "column",  height: "100vh",alignItems: "center",gap: "1.5rem",justifyContent: "center"}}>
-      
+    <div style={{color:"white", display: "flex",flexDirection: "column",  height: "100vh",alignItems: "center",gap: "1.5rem",justifyContent: "center"}}>
+      <div style={{backgroundColor: "rgba(0, 0, 0, 0.7)", padding: "20px", borderRadius: "10px",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)", maxWidth: "580px", margin: "0 auto",}}>
       <h1 style={{fontSize: "50px", fontStyle: "italic", fontFamily: "Bastligen"}}>
         <center>Fatih Cem</center>
       </h1>
       
       {/* Pass the image paths to the client component */}
       <RandomImage imagePaths={imagePaths} />
-      
+      </div>
       <div style={{
         width: "80%",
         maxWidth: "700px",
